409a1a282d1d2ea80fbc95319156afa28fcf52cf
Ludovic Henry <ludovichenry.utbm@gmail.com>
2012-02-13 15:00:30 -0800

    Add support to SHOW_DEBUG_TOOLBAR to enable or not the toolbar on requests

--- lib/web-debug-toolbar.rb
+++ lib/web-debug-toolbar.rb
@@ -10,37 +10,43 @@ module WebDebugToolbar
   toolbar.add_panel(ViewTimesPanel.new)
 
   ActiveSupport.on_load(:after_initialize) do
-    ActiveSupport::Notifications.subscribe do |name, start, finish, id, payload|
-      toolbar.notify(name, start, finish, id, payload)
+    if SHOW_DEBUG_TOOLBAR
+      ActiveSupport::Notifications.subscribe do |name, start, finish, id, payload|
+        toolbar.notify(name, start, finish, id, payload)
+      end
     end
   end
 
   ActiveSupport.on_load(:action_controller) do
     append_around_filter { |controller, action|
-      request_time_start = Time.now
+      if SHOW_DEBUG_TOOLBAR
+        request_time_start = Time.now
 
-      action.call
+        action.call
 
-      toolbar.notify('process_request.around_filter_end', request_time_start, Time.now, 0, [])
+        toolbar.notify('process_request.around_filter_end', request_time_start, Time.now, 0, [])
 
-      javascript = File.open(File.expand_path('web-debug-toolbar/assets/web-debug-toolbar.js', File.dirname(__FILE__)), 'r') do |file|
-        file.readlines.join
+        javascript = File.open(File.expand_path('web-debug-toolbar/assets/web-debug-toolbar.js', File.dirname(__FILE__)), 'r') do |file|
+          file.readlines.join
+        end
+
+        css = File.open(File.expand_path('web-debug-toolbar/assets/web-debug-toolbar.css', File.dirname(__FILE__)), 'r') do |file|
+          file.readlines.join
+        end
+
+        ## Create the box at the top right of the page and put it just before </body>
+        body = controller.response.body
+        body.sub! /<\/head>/, "<style type=\"text/css\" media=\"screen\" charset=\"utf-8\">#{css}</style><script type=\"text/javascript\" charset=\"utf-8\">#{javascript}</script></head>"
+        body.sub! /<\/body>/, "#{toolbar.render}</body>"
+
+        ## Update content-length header and response body content
+        controller.response["Content-Length"] = body.size.to_s
+        controller.response.body = body
+
+        toolbar.reset(controller)
+      else
+        action.call
       end
-
-      css = File.open(File.expand_path('web-debug-toolbar/assets/web-debug-toolbar.css', File.dirname(__FILE__)), 'r') do |file|
-        file.readlines.join
-      end
-
-      ## Create the box at the top right of the page and put it just before </body>
-      body = controller.response.body
-      body.sub! /<\/head>/, "<style type=\"text/css\" media=\"screen\" charset=\"utf-8\">#{css}</style><script type=\"text/javascript\" charset=\"utf-8\">#{javascript}</script></head>"
-      body.sub! /<\/body>/, "#{toolbar.render}</body>"
-
-      ## Update content-length header and response body content
-      controller.response["Content-Length"] = body.size.to_s
-      controller.response.body = body
-
-      toolbar.reset(controller)
     }
   end
 end
