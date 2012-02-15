890ab2f7ca9c91a81a993e8916e27aefb4f73d5d
Ludovic Henry <ludovichenry.utbm@gmail.com>
2012-02-15 11:45:48 -0800

    Remove total time of sql queries and replace it with number of queries

--- lib/web-debug-toolbar/panels/sql-queries-panel.rb
+++ lib/web-debug-toolbar/panels/sql-queries-panel.rb
@@ -1,6 +1,6 @@
 require 'web-debug-toolbar/panel'
 
-SqlQuery = Struct.new(:sql, :binds, :duration)
+SqlQuery = Struct.new(:name, :sql, :binds, :duration)
 
 class SqlQueriesPanel < Panel
   def initialize
@@ -12,7 +12,7 @@ class SqlQueriesPanel < Panel
   end
 
   def notify(name, start, finish, id, payload)
-    @sql_queries << SqlQuery.new(payload[:sql], payload[:binds], ((finish - start) * 1000).round(1))
+    @sql_queries << SqlQuery.new(payload[:name], payload[:sql], payload[:binds], ((finish - start) * 1000).round(1))
   end
 
   def reset(request, response)
@@ -20,11 +20,9 @@ class SqlQueriesPanel < Panel
   end
 
   def render
-    sql_queries_total_duration = 0
-
-    @sql_queries.each do |query|
-      sql_queries_total_duration += query.duration
-    end
+    @sql_queries.sort! { |a, b| a.name.to_s <=> b.name.to_s }
+    
+    sql_queries_count = @sql_queries.length
 
     sql_queries_render = ''
 
@@ -32,14 +30,14 @@ class SqlQueriesPanel < Panel
       lines = file.readlines
 
       @sql_queries.each do |query|
-        sql_queries_render += lines.join('').gsub('#{sql}', query.sql).gsub('#{binds}', query.binds.nil? ? '[]' : query.binds.map{ |bind| bind[1].to_s }.to_s).gsub('#{duration}', "%0.1f" % query.duration)
+        sql_queries_render += lines.join('').gsub('#{name}', query.name.nil? ? '' : query.name).gsub('#{sql}', query.sql).gsub('#{binds}', query.binds.nil? ? '[]' : query.binds.map{ |bind| bind[1].to_s }.to_s).gsub('#{duration}', "%0.1f" % query.duration)
       end
     end
 
     sql_queries_panel_render = ''
 
     File.open(File.expand_path('../views/sql-queries-panel.html', File.dirname(__FILE__)), 'r') do |file|
-      sql_queries_panel_render = file.readlines.join('').gsub('#{sql_queries_total_duration}', "%0.1f" % sql_queries_total_duration).gsub('#{sql_queries}', sql_queries_render)
+      sql_queries_panel_render = file.readlines.join('').gsub('#{sql_queries_count}', sql_queries_count.to_s).gsub('#{sql_queries}', sql_queries_render)
     end
 
     return sql_queries_panel_render
